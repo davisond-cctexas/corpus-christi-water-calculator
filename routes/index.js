a1cc77757af834cd90a7b8f8ae82cd4d163cb7e7
davisond-cctexas <davisond@cctexasa.com>
2018-03-14 14:41:34 -0500

    npm mysql

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'velu12',
+        database : 'water_calc_db'
+    });
+
+    connection.connect()
+
+    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+        if (err) throw err
+
+        console.log('The solution is: ', rows[0].solution)
+    })
+
+    connection.end()
+
+  res.render('index', { title: 'corpus chrtii' });
 });
 
 module.exports = router;
