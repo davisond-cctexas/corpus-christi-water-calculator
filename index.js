4a6902e6ba0bb21b5c5506d32f6728e302fcf9e3
davisond-cctexas <davisond@cctexas>
2018-05-16 16:40:40 -0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var router = express.Router();
+var request = require("request");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var content_all = "http://web01-stg-cctx-app01.ccpd.net/contentexportall";
+// var content_all = "http://cms.cctexas.com/entity/departments";
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+var homepage_json = "";
+
+/* GET home page. */``
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Responsive-Front-End-Test' });
+});
+
+
+request({
+    url: content_all ,
+    json: true
+}, function (error, response, body) {
+    if(!error && response.statusCode === 200){
+        content_all  = body;
+
+        console.log(content_all .toString());
+    }
+
+    var content = [];
+    // console.log("2");
+    content_all.forEach(function (item) {
+
+        content= content.concat(item);
+    });
+
+    console.log(content.toString());
+
+
+
+
+    //
+    // app.locals.departments= departments;
+});
+
+
+
+module.exports = router;
